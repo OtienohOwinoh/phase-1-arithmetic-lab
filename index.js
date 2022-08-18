3865662d623707f9570e6c341ce1bbbcf00a6be3
OtienohOwinoh <coowinoh@gmail.com>
2022-08-18 09:59:41 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = (num1 * num2);
+
+console.log(multiply)
+
+let num3 = 22;
+let num4 = 9;
+let mod = (num3 % num4)
+
+console.log(mod)
+
+
+let max = Math.max (7,16,19,20);
+console.log(max);
+
+let random = Math.floor (Math.random() * 10) + 1;
+
+console.log(random)
